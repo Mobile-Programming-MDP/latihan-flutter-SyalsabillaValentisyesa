60679d3c6793e08516550d439f8eda3ba2d303d6
Syalsabilla Valentisyesa <syalsabillavalentisyesa_2327250054@mhs.mdp.ac.id>
2024-10-17 15:22:44 +0700

    add detail header

---
+++ wisataa_candi/lib/detail_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:wisataa_candi/models/candi.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    height: 300,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
