601472d3f3dce9adfa9e8b37dcaf722489a7d979
Syalsabilla Valentisyesa <syalsabillavalentisyesa_2327250054@mhs.mdp.ac.id>
2024-10-19 12:58:58 +0700

    lanjutan pertemuan 12 candi eaaaa

--- wisataa_candi/lib/main.dart
+++ wisataa_candi/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DetailScreen(
-        candi: candiList[8],
+        candi: candiList[0],
       ),
     );
   }
