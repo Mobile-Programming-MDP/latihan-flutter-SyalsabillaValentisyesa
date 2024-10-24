8ecafe4617c031757ef44e4b7541174dccae3232
NabilaSZ <nabilasalwazahrani_2226240133@mhs.mdp.ac.id>
2024-10-24 15:05:10 +0700

    profile screen weeellll

--- wisataa_candi/lib/main.dart
+++ wisataa_candi/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wisataa_candi/data/candi_data.dart';
 import 'package:wisataa_candi/detail_screen.dart';
+import 'package:wisataa_candi/profile_screen.dart';
+
+import 'profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DetailScreen(
-        candi: candiList[0],
-      ),
+      // home: DetailScreen(
+      //   candi: candiList[0],
+      // ),
+      home: ProfileScreen(),
     );
   }
 }
---
+++ wisataa_candi/lib/widgets/profile_info_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProfileInfoItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+  final bool showEditions;
+  final VoidCallback? onEditPressed;
+  final Color iconColor;
+
+  const ProfileInfoItem(
+      {super.key,
+      required this.icon,
+      required this.label,
+      required this.value,
+      this.showEditIcon = false,
+      this.onEditPressed,
+      required this.iconColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
